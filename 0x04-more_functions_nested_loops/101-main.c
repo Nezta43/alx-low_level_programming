fcf41dd10a8bb5dc39644a6a0c566e120b64719a
Nezta43 <nezta43@gmail.com>
2022-10-24 10:15:31 -0700

    more

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <limits.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int_min
 int main(void)
 {
     print_number(98);
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints an integer using _putchar function,
+ * without using long data type, arrays or pointers
+ * @n: number to be printed
+ *
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
+		num = -num;
 	}
 
-	if (n1 / 10)
+	if ((num / 10) > 0)
 	{
-		print_number(n1 / 10);
+		print_number(num / 10);
 	}
 
-	_putchar((n1 % 10) + '0');
+	_putchar((num % 10) + '0');
+
 }
